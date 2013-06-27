eadce015a67cb62f302648db99887800e66b5b4b
Bugs Bunny <andrey.goryachev@gmail.com>
2013-06-27 19:20:59 +0400

    Example of using of services

---
+++ examples/service_client.cpp
@@ -0,0 +1,46 @@
+#include <cocaine/framework/services/application.hpp>
+#include <cocaine/framework/services/storage.hpp>
+
+#include <iostream>
+
+namespace cf = cocaine::framework;
+
+void
+handler(cf::future<std::string>& f)
+{
+    try {
+        std::cout << "result: " << f.get() << std::endl;
+    } catch (std::exception e) {
+        std::cout << "error: " << e.what() << std::endl;
+    }
+}
+
+int
+main(int argc,
+     char *argv[])
+{
+    auto manager = cf::service_manager_t::create(
+        cocaine::io::tcp::endpoint("127.0.0.1", 10053), // locator
+        "example" // use cocaine logger with source "example"
+    );
+
+    // call application
+    auto app = manager->get_service<cf::application_client_t>("app1");
+    auto g = app->enqueue("event5", "test"); // get generator
+
+    g.map(&handler); // call handler for each chunk
+
+    // or do call for first chunk:
+    // g.then([](cocaine::framework::generator<std::string>& g){std::string result = g.next(); g.map(&handler);});
+
+    // or synchronously get next chunk: std::string result = g.next();
+
+    // call storage service
+    auto storage = manager->get_service<cf::storage_service_t>("storage");
+    auto f = storage->read("collection", "key"); // get future
+
+    f.then(&handler); // call handler when future is ready
+    // or std::string result = f.get();
+
+    return 0;
+}
