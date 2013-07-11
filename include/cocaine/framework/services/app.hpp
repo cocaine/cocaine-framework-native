fcc9e23a87b56cda96367a39a33ef0ae74c18988
Andrey Sibiryov <me@kobology.ru>
2013-07-11 20:27:07 +0400

    Always pack the messages targeting App Services.

--- include/cocaine/framework/services/app.hpp
+++ include/cocaine/framework/services/app.hpp
@@ -10,19 +10,34 @@ namespace cocaine { namespace framework {
 struct app_service_t :
     public service_t
 {
-    static const unsigned int version = cocaine::io::protocol<cocaine::io::app_tag>::version::value;
+    static const unsigned int version = io::protocol<io::app_tag>::version::value;
 
     app_service_t(std::shared_ptr<service_connection_t> connection) :
         service_t(connection)
     {
-        // pass
+        // Pass.
     }
 
-    service_traits<cocaine::io::app::enqueue>::future_type
-    enqueue(const std::string& event,
-            const std::string& chunk)
-    {
-        return call<cocaine::io::app::enqueue>(event, chunk);
+    template<class T>
+    service_traits<io::app::enqueue>::future_type
+    enqueue(const std::string& event, const T& chunk) {
+        msgpack::sbuffer buffer;
+        msgpack::packer<msgpack::sbuffer> packer(buffer);
+
+        io::type_traits<T>::pack(packer, chunk);
+
+        return call<io::app::enqueue>(event, io::literal { buffer.data(), buffer.size() });
+    }
+
+    template<class T>
+    service_traits<io::app::enqueue>::future_type
+    enqueue(const std::string& event, const T& chunk, const std::string& tag) {
+        msgpack::sbuffer buffer;
+        msgpack::packer<msgpack::sbuffer> packer(buffer);
+
+        io::type_traits<T>::pack(packer, chunk);
+
+        return call<io::app::enqueue>(event, io::literal { buffer.data(), buffer.size() }, tag);
     }
 };
 
