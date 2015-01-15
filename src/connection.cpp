6e0d8a7a1ebcef69ef39bba1151e2d14eab6fbbc
Evgeny Safronov <division494@gmail.com>
2015-01-15 17:07:08 +0300

    [Dev] No need to be shared pointers, it's movable.

--- src/connection.cpp
+++ src/connection.cpp
@@ -13,12 +13,13 @@ bool connection_t::connected() const noexcept {
 }
 
 future_t<void> connection_t::connect(const endpoint_t& endpoint)  {
-    auto promise = std::make_shared<promise_t<void>>(); // TODO: shared?
+    promise_t<void> promise;
+    auto future = promise.get_future();
 
     std::lock_guard<std::mutex> lock(connection_queue_mutex);
     switch (state.load()) {
     case state_t::disconnected: {
-        connection_queue.push_back(promise);
+        connection_queue.emplace_back(std::move(promise));
         state = state_t::connecting;
         // TODO: If the socket is broken it should be reinitialized under the mutex.
         socket.async_connect(
@@ -28,19 +29,19 @@ future_t<void> connection_t::connect(const endpoint_t& endpoint)  {
         break;
     }
     case state_t::connecting: {
-        connection_queue.push_back(promise);
+        connection_queue.emplace_back(std::move(promise));
         break;
     }
     case state_t::connected: {
          BOOST_ASSERT(connection_queue.empty());
-         promise->set_value();
+         promise.set_value();
         break;
     }
     default:
         BOOST_ASSERT(false);
     }
 
-    return promise->get_future();
+    return future;
 }
 
 void connection_t::on_connected(const boost::system::error_code& ec) {
@@ -49,14 +50,14 @@ void connection_t::on_connected(const boost::system::error_code& ec) {
     std::lock_guard<std::mutex> lock(connection_queue_mutex);
     if (ec) {
         state = state_t::disconnected;
-        for (auto promise : connection_queue) {
-            promise->set_exception(boost::system::system_error(ec));
+        for (auto& promise : connection_queue) {
+            promise.set_exception(boost::system::system_error(ec));
         }
         connection_queue.clear();
     } else {
         state = state_t::connected;
-        for (auto promise : connection_queue) {
-            promise->set_value();
+        for (auto& promise : connection_queue) {
+            promise.set_value();
         }
         connection_queue.clear();
     }
