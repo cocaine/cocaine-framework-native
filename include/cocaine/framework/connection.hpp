a07181daad5a5c3cfc43c0a9c77d84e128200fc1
Evgeny Safronov <division494@gmail.com>
2015-01-15 17:03:47 +0300

    [Dev] Code decomposition.
    Connection implementation now lives in a separate translation unit.

--- include/cocaine/framework/connection.hpp
+++ include/cocaine/framework/connection.hpp
@@ -50,70 +50,18 @@ public:
      * \note the event loop reference should be valid until all asynchronous operations complete
      * otherwise the behavior is undefined.
      */
-    connection_t(loop_t& loop) noexcept :
-        loop(loop),
-        socket(loop),
-        state(state_t::disconnected)
-    {}
-
-    future_t<void> connect(const endpoint_t& endpoint) {
-        auto promise = std::make_shared<promise_t<void>>(); // TODO: shared?
-
-        std::lock_guard<std::mutex> lock(connection_queue_mutex);
-        switch (state.load()) {
-        case state_t::disconnected: {
-            connection_queue.push_back(promise);
-            state = state_t::connecting;
-            // TODO: If the socket is broken it should be reinitialized under the mutex.
-            socket.async_connect(
-                endpoint,
-                std::bind(&connection_t::on_connected, this->shared_from_this(), std::placeholders::_1)
-            );
-            break;
-        }
-        case state_t::connecting: {
-            connection_queue.push_back(promise);
-            break;
-        }
-        case state_t::connected: {
-             BOOST_ASSERT(connection_queue.empty());
-             promise->set_value();
-            break;
-        }
-        default:
-            BOOST_ASSERT(false);
-        }
-
-        return promise->get_future();
-    }
+    connection_t(loop_t& loop) noexcept;
 
     /*!
      * \note the class does passive connection monitoring, e.g. it won't be immediately notified
      * if the real connection has been lost, but after the next send/recv attempt.
      */
-    bool connected() const noexcept {
-        return state == state_t::connected;
-    }
+    bool connected() const noexcept;
+
+    future_t<void> connect(const endpoint_t& endpoint);
 
 private:
-    void on_connected(const boost::system::error_code& ec) {
-        // This callback can be called from any thread. The following mutex is guaranteed not to be
-        // locked at that moment.
-        std::lock_guard<std::mutex> lock(connection_queue_mutex);
-        if (ec) {
-            state = state_t::disconnected;
-            for (auto promise : connection_queue) {
-                promise->set_exception(boost::system::system_error(ec));
-            }
-            connection_queue.clear();
-        } else {
-            state = state_t::connected;
-            for (auto promise : connection_queue) {
-                promise->set_value();
-            }
-            connection_queue.clear();
-        }
-    }
+    void on_connected(const boost::system::error_code& ec);
 };
 
 } // namespace framework
---
+++ src/connection.cpp
@@ -0,0 +1,63 @@
+#include "connection.hpp"
+
+using namespace cocaine::framework;
+
+connection_t::connection_t(loop_t& loop) noexcept :
+    loop(loop),
+    socket(loop),
+    state(state_t::disconnected)
+{}
+
+bool connection_t::connected() const noexcept {
+    return state == state_t::connected;
+}
+
+future_t<void> connection_t::connect(const endpoint_t& endpoint)  {
+    auto promise = std::make_shared<promise_t<void>>(); // TODO: shared?
+
+    std::lock_guard<std::mutex> lock(connection_queue_mutex);
+    switch (state.load()) {
+    case state_t::disconnected: {
+        connection_queue.push_back(promise);
+        state = state_t::connecting;
+        // TODO: If the socket is broken it should be reinitialized under the mutex.
+        socket.async_connect(
+            endpoint,
+            std::bind(&connection_t::on_connected, this->shared_from_this(), std::placeholders::_1)
+        );
+        break;
+    }
+    case state_t::connecting: {
+        connection_queue.push_back(promise);
+        break;
+    }
+    case state_t::connected: {
+         BOOST_ASSERT(connection_queue.empty());
+         promise->set_value();
+        break;
+    }
+    default:
+        BOOST_ASSERT(false);
+    }
+
+    return promise->get_future();
+}
+
+void connection_t::on_connected(const boost::system::error_code& ec) {
+    // This callback can be called from any thread. The following mutex is guaranteed not to be
+    // locked at that moment.
+    std::lock_guard<std::mutex> lock(connection_queue_mutex);
+    if (ec) {
+        state = state_t::disconnected;
+        for (auto promise : connection_queue) {
+            promise->set_exception(boost::system::system_error(ec));
+        }
+        connection_queue.clear();
+    } else {
+        state = state_t::connected;
+        for (auto promise : connection_queue) {
+            promise->set_value();
+        }
+        connection_queue.clear();
+    }
+}
