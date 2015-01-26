eaee65f8596aa27d137734e30075d464a144bd96
Evgeny Safronov <division494@gmail.com>
2015-01-26 15:21:16 +0300

    [Dev] Decomposition, testing and docs.

--- include/cocaine/framework/connection.hpp
+++ include/cocaine/framework/connection.hpp
@@ -11,6 +11,8 @@
 #include "cocaine/framework/common.hpp"
 #include "cocaine/framework/config.hpp"
 
+namespace io_provider = asio;
+
 namespace cocaine {
 
 namespace framework {
@@ -18,13 +20,15 @@ namespace framework {
 /*!
  * \brief The basic_connection_t class
  *
- * \thread_safety unsafe.
+ * \thread_safety unsafe - all methods of this class **must** be called from the event loop thread,
+ * otherwise the behavior is undefined.
+ * \visibility internal usage only.
  */
 class basic_connection_t : public std::enable_shared_from_this<basic_connection_t> {
     typedef std::function<void(const std::error_code&)> callback_type;
 
 public:
-    typedef asio::ip::tcp protocol_type;
+    typedef io_provider::ip::tcp protocol_type;
     typedef protocol_type::socket socket_type;
     typedef io::channel<protocol_type> channel_type;
 
@@ -39,60 +43,21 @@ public:
     std::unique_ptr<channel_type> channel;
 
 public:
-    basic_connection_t(loop_t& loop) :
-        loop(loop),
-        state(state_t::disconnected)
-    {}
+    basic_connection_t(loop_t& loop);
 
     basic_connection_t(basic_connection_t&& other) = default;
     basic_connection_t& operator=(basic_connection_t&& other) = default;
 
-    bool connected() const noexcept {
-        return state == state_t::connected;
-    }
+    bool connected() const;
+    void connect(const endpoint_t& endpoint, callback_type callback);
+    void disconnect();
 
-    void connect(const endpoint_t& endpoint, callback_type callback) {
-        switch (state) {
-        case state_t::disconnected: {
-            std::unique_ptr<socket_type> socket(new socket_type(loop));
-            socket->open(endpoint.protocol());
-
-            channel = std::unique_ptr<channel_type>(new channel_type(std::move(socket)));
-            channel->socket->async_connect(
-                endpoint,
-                std::bind(&basic_connection_t::on_connect, shared_from_this(), std::placeholders::_1, callback)
-            );
-
-            // The code above can throw std::bad_alloc, so here it is the right place to change
-            // current object's state.
-            state = state_t::connecting;
-            break;
-        }
-        case state_t::connecting: {
-            loop.dispatch(std::bind(callback, asio::error::in_progress));
-            break;
-        }
-        case state_t::connected: {
-            loop.dispatch(std::bind(callback, asio::error::already_connected));
-            break;
-        }
-        default:
-            COCAINE_ASSERT(false);
-        }
-    }
-
-    void disconnect() {}
-
-    void read(callback_type callback) {}
-    void write(callback_type callback) {}
+    void read(io::decoder_t::message_type& message, callback_type callback);
+    void write(const io::encoder_t::message_type& message, callback_type callback);
 
 private:
-    void on_connect(const std::error_code& ec, callback_type callback) {
-        COCAINE_ASSERT(state == state_t::connecting);
-
-        state = ec ? state_t::disconnected : state_t::connected;
-        callback(ec);
-    }
+    void on_connect(const std::error_code& ec, callback_type callback);
+    void on_readwrite(const std::error_code& ec, callback_type callback);
 };
 
 }
--- src/connection.cpp
+++ src/connection.cpp
@@ -1,1 +1,94 @@
 #include "cocaine/framework/connection.hpp"
+
+namespace ph = std::placeholders;
+
+using namespace cocaine::framework;
+
+basic_connection_t::basic_connection_t(loop_t& loop) :
+    loop(loop),
+    state(state_t::disconnected)
+{}
+
+bool basic_connection_t::connected() const {
+    return state == state_t::connected;
+}
+
+void basic_connection_t::connect(const endpoint_t& endpoint, callback_type callback) {
+    switch (state) {
+    case state_t::disconnected: {
+        std::unique_ptr<socket_type> socket(new socket_type(loop));
+        socket->open(endpoint.protocol());
+
+        channel.reset(new channel_type(std::move(socket)));
+        channel->socket->async_connect(
+            endpoint,
+            std::bind(&basic_connection_t::on_connect, shared_from_this(), std::placeholders::_1, callback)
+        );
+
+        // The code above can throw std::bad_alloc, so here it is the right place to change
+        // current object's state.
+        state = state_t::connecting;
+        break;
+    }
+    case state_t::connecting: {
+        loop.dispatch(std::bind(callback, asio::error::in_progress));
+        break;
+    }
+    case state_t::connected: {
+        loop.dispatch(std::bind(callback, asio::error::already_connected));
+        break;
+    }
+    default:
+        COCAINE_ASSERT(false);
+    }
+}
+
+void basic_connection_t::disconnect() {
+    state = state_t::disconnected;
+    channel.reset();
+}
+
+void basic_connection_t::read(io::decoder_t::message_type& message, callback_type callback) {
+    if (channel) {
+        channel->reader->read(
+            message,
+            std::bind(&basic_connection_t::on_readwrite, shared_from_this(), ph::_1, std::move(callback))
+        );
+    } else {
+        loop.post(std::bind(callback, io_provider::error::not_connected));
+    }
+}
+
+void basic_connection_t::write(const io::encoder_t::message_type& message, callback_type callback) {
+    if (channel) {
+        channel->writer->write(
+            message,
+            std::bind(&basic_connection_t::on_readwrite, shared_from_this(), ph::_1, std::move(callback))
+        );
+    } else {
+        loop.post(std::bind(callback, io_provider::error::not_connected));
+    }
+}
+
+void basic_connection_t::on_connect(const std::error_code& ec, callback_type callback)  {
+    COCAINE_ASSERT(channel);
+    COCAINE_ASSERT(state == state_t::connecting);
+
+    if (ec) {
+        state = state_t::disconnected;
+        channel.reset();
+    } else {
+        state = state_t::connected;
+    }
+
+    callback(ec);
+}
+
+void basic_connection_t::on_readwrite(const std::error_code& ec, callback_type callback) {
+    if (ec) {
+        state = state_t::disconnected;
+        channel.reset();
+    }
+
+    callback(ec);
+}
