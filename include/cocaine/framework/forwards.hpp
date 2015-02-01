ed839775ffada5aacf47018827f632470ed4ab9e
Evgeny Safronov <division494@gmail.com>
2015-02-01 14:59:42 +0300

    [Dev] More forwards.

--- include/cocaine/framework/forwards.hpp
+++ include/cocaine/framework/forwards.hpp
@@ -1,10 +1,14 @@
 #pragma once
 
+#include <asio/io_service.hpp>
+
 #include "cocaine/framework/util/future.hpp"
 
 namespace cocaine {
 
 namespace framework {
+
+using loop_t = asio::io_service;
 
 template<typename T>
 using promise_t = promise<T>;
