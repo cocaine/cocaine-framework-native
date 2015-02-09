02734a621fcbd594b8d9a1b2c77a8c32c7691684
Evgeny Safronov <division494@gmail.com>
2015-02-09 16:23:23 +0300

    [Dev] Health skeleton.

---
+++ include/cocaine/framework/health.hpp
@@ -0,0 +1,30 @@
+//#pragma once
+
+//namespace cocaine {
+
+//namespace framework {
+
+//class health_t {
+//public:
+//    health_t(loop_t& loop, std::shared_ptr<worker_session_t> session) {
+//        // start timers
+//    }
+
+//    ~health_t() {
+//        // stop timers
+//    }
+
+//    // on heartbeat message
+//    void inhale() {
+//        // reset disown timer
+//    }
+
+//private:
+//    void exhale() {} // on heartbeat timer, send message via session.
+//    void on_death() {} // on disown timer. disconnect session.
+
+//};
+
+//}
+
+//}
