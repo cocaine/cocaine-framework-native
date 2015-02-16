8a0462a104b056a8773f8e332c6fa71b44fc4dc0
Evgeny Safronov <division494@gmail.com>
2015-02-16 18:29:15 +0300

    [Dev] Service manager.

---
+++ include/cocaine/framework/manager.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <memory>
+
+#include <boost/optional.hpp>
+#include <boost/thread.hpp>
+
+#include "cocaine/framework/forwards.hpp"
+
+namespace cocaine {
+
+namespace framework {
+
+class service_manager_t {
+    loop_t loop;
+    boost::optional<loop_t::work> work;
+    boost::thread_group pool;
+
+public:
+    service_manager_t();
+    explicit service_manager_t(unsigned int threads);
+
+    ~service_manager_t();
+
+    template<class Service, class... Args>
+    std::shared_ptr<Service> create(const std::string& name) {
+        return std::make_shared<Service>(name, loop);
+    }
+
+private:
+    void start(unsigned int threads);
+};
+
+}
+
+} // namespace cocaine::framework
---
+++ src/manager.cpp
@@ -0,0 +1,34 @@
+#include "cocaine/framework/manager.hpp"
+
+using namespace cocaine::framework;
+
+service_manager_t::service_manager_t() :
+    work(loop)
+{
+    auto threads = boost::thread::hardware_concurrency();
+    start(threads != 0 ? threads : 1);
+}
+
+service_manager_t::service_manager_t(unsigned int threads) :
+    work(loop)
+{
+    if (threads == 0) {
+        throw std::invalid_argument("thread count must be a positive number");
+    }
+
+    start(threads);
+}
+
+service_manager_t::~service_manager_t() {
+    work.reset();
+    loop.stop();
+    pool.join_all();
+}
+
+void service_manager_t::start(unsigned int threads) {
+    for (unsigned int i = 0; i < threads; ++i) {
+        pool.create_thread(
+            std::bind(static_cast<std::size_t(loop_t::*)()>(&loop_t::run), std::ref(loop))
+        );
+    }
+}
