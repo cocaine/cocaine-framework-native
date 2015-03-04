fd1650490fc4fa28fb258003ebc21da2dbddd959
Evgeny Safronov <division494@gmail.com>
2015-03-04 18:14:23 +0300

    [Dev] Async service connect.

--- src/service.cpp
+++ src/service.cpp
@@ -3,8 +3,36 @@
 #include "cocaine/framework/detail/basic_session.hpp"
 #include "cocaine/framework/detail/resolver.hpp"
 
+namespace ph = std::placeholders;
+
 using namespace cocaine::framework;
 using namespace cocaine::framework::detail;
+
+namespace {
+
+typename task<void>::future_type
+on_resolve(typename task<resolver_t::result_t>::future_move_type future, uint version, std::shared_ptr<session_t> session) {
+    auto info = future.get();
+    if (version != info.version) {
+        // TODO: Typed exceptions.
+        return make_ready_future<void>::error(std::runtime_error("version mismatch"));
+    }
+
+    return session->connect(info.endpoints);
+}
+
+void
+on_connect(typename task<void>::future_move_type future) {
+    try {
+        future.get();
+        CF_DBG("connected");
+    } catch (const std::exception& err) {
+        CF_DBG("failed to connect: %s", err.what());
+        throw;
+    }
+}
+
+}
 
 class basic_service_t::impl {
 public:
@@ -42,7 +70,6 @@ auto basic_service_t::connect() -> typename task<void>::future_type {
     CF_CTX("SC");
     CF_DBG("connecting ...");
 
-    // TODO: Make async.
     std::lock_guard<std::mutex> lock(d->mutex);
 
     // Internally the session manages with connection state itself. On any network error it
@@ -52,19 +79,9 @@ auto basic_service_t::connect() -> typename task<void>::future_type {
         return make_ready_future<void>::value();
     }
 
-    try {
-        const auto info = d->resolver.resolve(d->name).get();
-        if (d->version != info.version) {
-            return make_ready_future<void>::error(std::runtime_error("version mismatch"));
-        }
-
-        session->connect(info.endpoints).get();
-        CF_DBG("connected");
-        return make_ready_future<void>::value();
-    } catch (const std::runtime_error& err) {
-        CF_DBG("failed to connect: %s", err.what());
-        return make_ready_future<void>::error(err);
-    }
+    return d->resolver.resolve(d->name)
+        .then(wrap(std::bind(&::on_resolve, ph::_1, d->version, session)))
+        .then(wrap(std::bind(&::on_connect, ph::_1)));
 }
 
 auto basic_service_t::endpoint() const -> boost::optional<session_t::endpoint_type> {
