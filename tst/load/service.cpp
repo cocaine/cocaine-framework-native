40424c6eb0d3082684d46cbfb658f9a778ba8f63
Evgeny Safronov <division494@gmail.com>
2015-03-10 14:23:35 +0300

    [UT] Continue to make options configurable.

--- tst/load/service.cpp
+++ tst/load/service.cpp
@@ -26,9 +26,12 @@ T get_option(const char* name, T def) {
 
 } // namespace testing
 
+static const char DEFAULT_APP[] = "echo-cpp";
+static const uint DEFAULT_ITERS = 10000;
+
 TEST(load, EchoSyncST) {
-    const std::string APP = testing::get_option<std::string>("load.EchoSyncST.app", "echo-cpp");
-    const uint ITERS = testing::get_option<uint>("load.EchoSyncST.iters", 10000);
+    const std::string APP = testing::get_option<std::string>("load.EchoSyncST.app", DEFAULT_APP);
+    const uint ITERS = testing::get_option<uint>("load.EchoSyncST.iters", DEFAULT_ITERS);
 
     service_manager_t manager(1);
     auto echo = manager.create<cocaine::io::app_tag>(APP);
@@ -96,12 +99,13 @@ on_invoke(typename task<typename invocation_result<io::app::enqueue>::type>::fut
 } // namespace
 
 TEST(load, EchoAsyncST) {
-    const int ITERS = 1000;
+    const std::string APP = testing::get_option<std::string>("load.EchoAsyncST.app", DEFAULT_APP);
+    const uint ITERS = testing::get_option<uint>("load.EchoAsyncST.iters", DEFAULT_ITERS);
 
     std::atomic<int> counter(0);
 
     service_manager_t manager(1);
-    auto echo = manager.create<cocaine::io::app_tag>("echo-cpp");
+    auto echo = manager.create<cocaine::io::app_tag>(APP);
 
     std::vector<typename task<void>::future_type> futures;
     futures.reserve(ITERS);
