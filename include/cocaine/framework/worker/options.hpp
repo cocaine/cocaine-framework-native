bf0a16610bb4f198b4472b1f23f5c338bca91b9f
Evgeny Safronov <division494@gmail.com>
2015-03-17 13:22:53 +0300

    [Worker] More documentation.

--- include/cocaine/framework/worker/options.hpp
+++ include/cocaine/framework/worker/options.hpp
@@ -12,6 +12,11 @@ struct options_t {
     std::string endpoint;
     std::string locator;
 
+    /// Parses command-line arguments to extract all required settings to be able to start the
+    /// worker.
+    ///
+    /// Can internally exit on invalid command-line arguments, providing an help message and
+    /// returning proper exit code.
     options_t(int argc, char** argv);
 };
 
