4e11ca186e05783934328b2d16efa4827f737ff4
Evgeny Safronov <division494@gmail.com>
2015-03-25 12:12:44 +0300

    [UT] Improving load testing.

--- tst/load/ab.cpp
+++ tst/load/ab.cpp
@@ -1,13 +1,18 @@
 #include <gtest/gtest.h>
 
+#include <fstream>
+
 #include <boost/accumulators/accumulators.hpp>
 #include <boost/accumulators/statistics.hpp>
 #include <boost/accumulators/statistics/density.hpp>
+#include <boost/thread.hpp>
 
 #include <cocaine/idl/node.hpp>
 
 #include <cocaine/framework/service.hpp>
 #include <cocaine/framework/manager.hpp>
+
+#include "../util/env.hpp"
 
 namespace ph = std::placeholders;
 
@@ -67,7 +72,10 @@ on_invoke(task<invocation_result<io::app::enqueue>::type>::future_move_type futu
         .then(std::bind(&on_choke, ph::_1, std::ref(counter)));
 }
 
-void on_end(task<void>::future_move_type future, std::chrono::high_resolution_clock::time_point start, uint i, acc_t& acc) {
+void on_end(task<void>::future_move_type future,
+            std::chrono::high_resolution_clock::time_point start,
+            acc_t& acc)
+{
     auto now = std::chrono::high_resolution_clock::now();
 
     auto elapsed = std::chrono::duration<float, std::chrono::milliseconds::period>(now - start).count();
@@ -75,11 +83,81 @@ void on_end(task<void>::future_move_type future, std::chrono::high_resolution_cl
     future.get();
 }
 
-} // namespace ab
+namespace http {
 
-#include <asio.hpp>
-#include <boost/thread.hpp>
-#include <cocaine/framework/detail/loop.hpp>
+task<boost::optional<std::string>>::future_type
+on_send(task<sender<io::app::enqueue::dispatch_type, basic_session_t>>::future_move_type future,
+        receiver<io::app::enqueue::upstream_type, basic_session_t> rx)
+{
+    future.get();
+    return rx.recv();
+}
+
+task<boost::optional<std::string>>::future_type
+on_headers(task<boost::optional<std::string>>::future_move_type future,
+           receiver<io::app::enqueue::upstream_type, basic_session_t> rx)
+{
+    auto payload = future.get();
+
+    int code;
+    std::vector<std::pair<std::string, std::string>> headers;
+
+    msgpack::unpacked msg;
+    msgpack::unpack(&msg, payload->data(), payload->size());
+    std::tuple<int, decltype(headers)> obj;
+    io::type_traits<decltype(obj)>::unpack(msg.get(), obj);
+
+    std::tie(code, headers) = obj;
+    EXPECT_EQ(200, code);
+
+    return rx.recv();
+}
+
+task<boost::optional<std::string>>::future_type
+on_body(task<boost::optional<std::string>>::future_move_type future,
+        receiver<io::app::enqueue::upstream_type, basic_session_t> rx)
+{
+    auto payload = future.get();
+
+    msgpack::unpacked msg;
+    msgpack::unpack(&msg, payload->data(), payload->size());
+    std::string obj;
+    cocaine::io::type_traits<std::string>::unpack(msg.get(), obj);
+
+    return rx.recv();
+}
+
+void
+on_close(task<boost::optional<std::string>>::future_move_type future, std::atomic<int>& counter) {
+    auto result = future.get();
+    EXPECT_FALSE(result);
+
+    counter++;
+}
+
+task<void>::future_type
+on_invoke(task<invocation_result<io::app::enqueue>::type>::future_move_type future,
+          std::atomic<int>& counter)
+{
+    auto channel = future.get();
+    auto tx = std::move(channel.tx);
+    auto rx = std::move(channel.rx);
+
+    std::vector<std::pair<std::string, std::string>> headers;
+
+    msgpack::sbuffer buf;
+    msgpack::pack(buf, std::make_tuple(std::string("GET"), std::string("/"), std::string("1.1"), headers, std::string("")));
+
+    return tx.send<scope::chunk>(std::string(buf.data(), buf.size()))
+        .then(std::bind(&on_send, ph::_1, rx))
+        .then(std::bind(&on_headers, ph::_1, rx))
+        .then(std::bind(&on_body, ph::_1, rx))
+        .then(std::bind(&on_close, ph::_1, std::ref(counter)));
+}
+
+} // namespace http
+
+} // namespace ab
 
 TEST(load, ab) {
     const uint ITERS = 2000;
@@ -100,7 +178,7 @@ TEST(load, ab) {
         futures.emplace_back(
             echo.invoke<io::app::enqueue>(std::string("ping"))
                 .then(std::bind(&ab::on_invoke, ph::_1, std::ref(counter)))
-                .then(std::bind(&ab::on_end, ph::_1, now, i, std::ref(acc)))
+                .then(std::bind(&ab::on_end, ph::_1, now, std::ref(acc)))
         );
     }
 
@@ -117,6 +195,55 @@ TEST(load, ab) {
             boost::accumulators::quantile_probability = probability
         );
 
-        fprintf(stdout, "%6.3f : %6.3f\n", probability, val);
+        fprintf(stdout, "%6.1f%% : %6.3fms\n", 100 * probability, val);
+    }
+}
+
+TEST(load, ab_http) {
+    const std::string CONFIG_FILENAME = testing::util::get_option<std::string>("CF_CFG", "");
+
+    std::string app = "nodejs";
+    std::string event = "http";
+    uint iters = 100;
+
+    if (!CONFIG_FILENAME.empty()) {
+        std::ifstream infile(CONFIG_FILENAME);
+        infile >> app >> event >> iters;
+    }
+
+    acc_t acc(boost::accumulators::tag::tail<boost::accumulators::right>::cache_size = iters);
+
+    std::atomic<int> counter(0);
+
+    service_manager_t manager(1);
+    auto echo = manager.create<cocaine::io::app_tag>(app);
+    echo.connect().get();
+
+    std::vector<task<void>::future_type> futures;
+    futures.reserve(iters);
+
+    for (uint i = 0; i < iters; ++i) {
+        auto now = std::chrono::high_resolution_clock::now();
+        futures.emplace_back(
+            echo.invoke<io::app::enqueue>(event)
+                .then(std::bind(&ab::http::on_invoke, ph::_1, std::ref(counter)))
+                .then(std::bind(&ab::on_end, ph::_1, now, std::ref(acc)))
+        );
+    }
+
+    // Block here.
+    for (auto& future : futures) {
+        future.get();
+    }
+
+    EXPECT_EQ(iters, counter);
+
+    for (auto probability : { 0.50, 0.75, 0.90, 0.95, 0.98, 0.99, 0.9995 }) {
+        const double val = boost::accumulators::quantile(
+            acc,
+            boost::accumulators::quantile_probability = probability
+        );
+
+        fprintf(stdout, "%6.2f%% : %6.3fms\n", 100 * probability, val);
     }
 }
