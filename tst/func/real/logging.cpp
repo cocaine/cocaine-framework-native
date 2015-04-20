d9a4b924e87c0458049979e0cdbf5191979b449a
Evgeny Safronov <division494@gmail.com>
2015-04-20 11:07:05 +0300

    [UT] Testing logging service.

--- tst/func/real/logging.cpp
+++ tst/func/real/logging.cpp
@@ -4,7 +4,10 @@
 
 #include <cocaine/common.hpp>
 #include <cocaine/idl/logging.hpp>
+#include <cocaine/traits/attributes.hpp>
 #include <cocaine/traits/enum.hpp>
+#include <cocaine/traits/tuple.hpp>
+#include <cocaine/traits/vector.hpp>
 
 #include <cocaine/framework/manager.hpp>
 #include <cocaine/framework/service.hpp>
@@ -15,5 +18,12 @@ using namespace cocaine::framework;
 TEST(service, Logging) {
     service_manager_t manager(1);
     auto logger = manager.create<io::log_tag>("logging");
-    logger.invoke<io::log::emit>(logging::debug, std::string("app/testing"), std::string("le message")).get();
+    logger.invoke<io::log::emit>(logging::info, std::string("app/testing"), std::string("le message")).get();
+
+    logger.invoke<io::log::emit>(
+        logging::info,
+        std::string("app/testing"),
+        std::string("le message"),
+        blackhole::attribute::set_t({{ "key", blackhole::attribute::value_t(42) }})
+    ).get();
 }
