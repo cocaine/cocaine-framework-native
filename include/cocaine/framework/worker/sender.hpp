b233f2a150353f852b95763688927301fca6637b
Evgeny Safronov <division494@gmail.com>
2016-03-03 13:54:37 +0300

    docs(worker): add more documentation bullshit

--- include/cocaine/framework/worker/sender.hpp
+++ include/cocaine/framework/worker/sender.hpp
@@ -1,6 +1,6 @@
 /*
-    Copyright (c) 2015 Evgeny Safronov <division494@gmail.com>
-    Copyright (c) 2011-2015 Other contributors as noted in the AUTHORS file.
+    Copyright (c) 2015-2016 Evgeny Safronov <division494@gmail.com>
+    Copyright (c) 2011-2016 Other contributors as noted in the AUTHORS file.
     This file is part of Cocaine.
     Cocaine is free software; you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
@@ -24,34 +24,42 @@
 #include "cocaine/framework/forwards.hpp"
 
 namespace cocaine {
-
 namespace framework {
-
 namespace worker {
 
 class sender {
     std::shared_ptr<basic_sender_t<worker_session_t>> session;
 
 public:
-    /*!
-     * \note this constructor is intentionally left implicit.
-     */
+    /// \note this constructor is intentionally left implicit.
     sender(std::shared_ptr<basic_sender_t<worker_session_t>> session);
-    ~sender();
-
     sender(const sender& other) = delete;
     sender(sender&&) = default;
 
-    sender& operator=(const sender& other) = delete;
-    sender& operator=(sender&&) = default;
+    ~sender();
 
+    auto operator=(const sender& other) -> sender& = delete;
+    auto operator=(sender&&) -> sender& = default;
+
+    /// Writes the provided message into the associated channel.
+    ///
+    /// \warning this sender will be invalidated after this call. The proper signature should
+    ///     involve rvalue reference from this, but our compilers doesn't support it yey.
     auto write(std::string message) -> task<sender>::future_type;
+
+    /// Sends an error into the associated channel.
+    ///
+    /// \warning this sender will be invalidated after this call. The proper signature should
+    ///     involve rvalue reference from this, but our compilers doesn't support it yey.
     auto error(int id, std::string reason) -> task<void>::future_type;
+
+    /// Closes the channel.
+    ///
+    /// \warning this sender will be invalidated after this call. The proper signature should
+    ///     involve rvalue reference from this, but our compilers doesn't support it yey.
     auto close() -> task<void>::future_type;
 };
 
-} // namespace worker
-
-} // namespace framework
-
-} // namespace cocaine
+}  // namespace worker
+}  // namespace framework
+}  // namespace cocaine
